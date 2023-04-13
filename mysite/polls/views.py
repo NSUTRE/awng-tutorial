9cc8238fa68be25ae931fae8dfff5e249aeffc91
Nina Sutre <Nina.SUTRE@student.isae-supaero.fr>
2023-04-13 08:36:16 +0200

    ajout de la def get_querysetself)

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -7,6 +7,7 @@ from django.template import loader
 
 from .models import Choice, Question
 from django.http import Http404
+from django.utils import timezone
 
 
 
@@ -15,8 +16,13 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        """
+        Return the last five published questions (not including those set to be
+        published in the future).
+        """
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
